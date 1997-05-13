7bb0c59e451ab9817da763781a089113b5c04016
Felix Sebastian Gallo <fsg@ultranet.com>
1997-05-13 19:57:43 +0000

    Import of FSG/Penguin-3.00 from CPAN.
    gitpan-cpan-distribution: Penguin
    gitpan-cpan-version:      3.00
    gitpan-cpan-path:         FSG/Penguin-3.00.tar.gz
    gitpan-cpan-author:       FSG
    gitpan-cpan-maturity:     released

--- example-client.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-use Penguin;
-use PGP;
-
-PGP::setpassword("ants in my pants");
-print("sending code...\n");
-$y = Penguin::sendcode("print hello", "localhost", 5059);
-print("..sent.\n");
